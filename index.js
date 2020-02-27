dee01c3b7c94a1c2cc6737108d09a6175ef13bcb
Sophie <sophie88leigh@gmail.com>
2020-02-27 00:31:53 -0500

    install package.json, creating index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+//letters
+var letterArray = "abcdefghijklmnopqrstuvwxyz";
+
+//list of words to choose from
+var Words = [
+    "apple",
+    "asparagus",
+    "banana",
+    "bed",
+    "beets",
+]
+

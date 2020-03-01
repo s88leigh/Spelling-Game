0b83764f7ed98257badf9e09420e188566d4f5b8
Sophie <sophie88leigh@gmail.com>
2020-02-29 21:41:26 -0500

    registered bootstrap in main.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './custom.scss'
 
 
 new Vue({

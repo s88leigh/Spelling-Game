eef3bf02327c2cb4d764b3fe99bda88e6799c3eb
Sophie <sophie88leigh@gmail.com>
2020-03-02 22:15:58 -0500

    moved  page links to bottom of page

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,26 +8,18 @@
       <furniture />
       <colors />
     </router-view>
-    <ul>
-      <li>
-        <router-link to="/">home</router-link>
-      </li>
-      <li>
-        <router-link to="/fruits">fruits</router-link>
-      </li>
-      <li>
-        <router-link to="/vegetables">vegetables</router-link>
-      </li>
-      <li>
-        <router-link to="/animals">animals</router-link>
-      </li>
-      <li>
-        <router-link to="/furniture">furniture</router-link>
-      </li>
-      <li>
-        <router-link to="/colors">colors</router-link>
-      </li>
-    </ul>
+
+    <router-link to="/">home</router-link>
+
+    <router-link to="/fruits">fruits</router-link>
+
+    <router-link to="/vegetables">vegetables</router-link>
+
+    <router-link to="/animals">animals</router-link>
+
+    <router-link to="/furniture">furniture</router-link>
+
+    <router-link to="/colors">colors</router-link>
   </div>
 </template>
 
@@ -61,33 +53,8 @@ export default {
 <style >
 #app {
   width: 100%;
-  height: 800px;
+  height: 750px;
   background: radial-gradient(azure, lightskyblue);
 }
-.game-title {
-  text-align: center;
-  font-size: 3em;
-  margin-bottom: 15px;
-  margin-top: 0px;
-}
-h1 {
-  color: rgba(3, 162, 171, 0.898);
-  text-shadow: 2px 2px 4px #01172d;
-}
-
-h3 {
-  text-align: center;
-  margin-bottom: 40px;
-  color: rgb(34, 6, 96);
-  text-shadow: 1px 1px 2px white, 0 0 25px #f2f4f7, 0 0 5px #313539;
-}
-/* .icon-container {
-  margin-bottom: 0px;
-  margin-top: 300px;
-}
-.admin-icon {
-  width: 40px;
-  height: 40px;
-} */
 </style>>
 

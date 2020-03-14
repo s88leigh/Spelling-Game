7e7618c5b17d33e158a2b755a37192c6fcab329b
Sophie <sophie88leigh@gmail.com>
2020-03-14 12:56:13 -0400

    change background color of underscore to white

--- client/src/components/underscore.vue
+++ client/src/components/underscore.vue
@@ -27,6 +27,7 @@ export default {
   letter-spacing: 8px;
   font-size: 50px;
   color: black;
+  background: #f2f4f7;
   text-shadow: 1px 1px 2px white, 0 0 25px #f2f4f7, 0 0 5px #313539;
   border: 2px solid rgb(4, 4, 98);
 }
